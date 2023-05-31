1c3508e1858c84ce5ef31ffc0c05c828f61b4137
k2gutierrez <cchimal.gutierrez@gmail.com>
2023-05-31 17:06:59 -0600

    end of app crud with firebase with todo app

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;600;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <div id='portal'></div>
       </body>
     </Html>
   )
